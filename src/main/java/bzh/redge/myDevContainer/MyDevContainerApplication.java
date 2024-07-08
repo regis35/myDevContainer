c028d2ea446d38000781541f2a0594d2938ec12b
redge35000 <lecoz.regis.pro@gmail.com>
2024-07-08 20:51:29 +0200

    open port

---
+++ src/main/java/bzh/redge/myDevContainer/MyDevContainerApplication.java
@@ -0,0 +1,13 @@
+package bzh.redge.myDevContainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDevContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bzh/redge/myDevContainer/MyDevContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package bzh.redge.myDevContainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDevContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
